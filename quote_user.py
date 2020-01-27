05f771447642503ca8f52e53c11a521321f9dda4
vladyslav nykoliuk <37787869+vladyslavnUA@users.noreply.github.com>
2020-01-27 15:17:00 -0800

    updated quote user file

---
+++ quote_user.py
@@ -0,0 +1,9 @@
+from python_quote import random_python_quote as a_quote
+
+
+def random_python_quote():
+    return random_python_quote()
+
+if __name__ == '__main__':
+    quote = a_quote()
+    print(quote)
