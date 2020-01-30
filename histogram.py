9327b78de97ccebd896f60ee2c49a4242835a2ec
vladyslav nykoliuk <37787869+vladyslavnUA@users.noreply.github.com>
2020-01-29 16:34:51 -0800

    updated histogram

--- histogram.py
+++ histogram.py
@@ -5,13 +5,29 @@ some_file = "words.txt"
 some_lines = open(some_file, "r")
 
 histoogram = {}
+def histogram():
+    for word in some_lines:
+        word = word.rstrip()
+    
+        if word in histoogram:
+            histoogram[word] += 1
+        else:
+            histoogram[word] = 1
 
-for word in some_lines:
-  word = word.rstrip()
-  
-  if word in histoogram:
-    histoogram[word] += 1
-  else:
-    histoogram[word] = 1
+    print(histoogram)
 
-print(histoogram)
+def unique_words():
+    count = 0
+    if word in histoogram:
+        #check if word is unique, only seen once
+        if histoogram[word] == 1:
+            count += 1
+    return(count)
+
+def frequency():
+    for word in histoogram:
+        print(f'the word {word} is repeated {histoogram[word]} times.')
+
+if __name__ == '__main__':
+    print(histogram())
+    print(frequency())
