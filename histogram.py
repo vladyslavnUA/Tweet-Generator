ccd9fdbbdb8e097f8c0f620a2e09d5e2da3d864c
vladyslavnUA <vladyslavn@yahoo.com>
2020-02-03 15:32:29 -0800

    updated files

--- histogram.py
+++ histogram.py
@@ -1,32 +1,33 @@
 import random
 import sys
 
-# filen = open("/Users/vladyslav/Desktop/cs12/somewords.txt", "r")
-# some_lines = filen.readlines()
-
-# histoogram = {}
-
-# def histogram():
-#     for any_word in some_lines:
-#         any_word = any_word.rstrip()
-#         histoogram[any_wordouch ] = histoogram.get(word, 0) + 1
-#     return histoogram
-# def unique_words():
-#     c = 0
-#     for any_word in histoogram:
-#         print(int(i))
-
 some_file = "words.txt"
 some_lines = open(some_file, "r")
 
 histoogram = {}
+def histogram():
+    for word in some_lines:
+        word = word.rstrip()
+    
+        if word in histoogram:
+            histoogram[word] += 1
+        else:
+            histoogram[word] = 1
 
-for word in some_lines:
-  word = word.rstrip()
-  
-  if word in histoogram:
-    histoogram[word] += 1
-  else:
-    histoogram[word] = 1
+    print(histoogram)
 
-print(histoogram)
+def unique_words():
+    count = 0
+    if word in histoogram:
+        #check if word is unique, only seen once
+        if histoogram[word] == 1:
+            count += 1
+    return(count)
+
+def frequency():
+    for word in histoogram:
+        print(f'the word {word} is repeated {histoogram[word]} times.')
+
+if __name__ == '__main__':
+    print(histogram())
+    print(frequency())
