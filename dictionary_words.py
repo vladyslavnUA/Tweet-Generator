8fdf37c3390f70e8e58f0f1b863fd862361fe10b
vladyslav nykoliuk <37787869+vladyslavnUA@users.noreply.github.com>
2020-02-03 15:35:38 -0800

    simplified dictionary words

--- dictionary_words.py
+++ dictionary_words.py
@@ -1,22 +1,12 @@
 import sys
 from random import random, randint
 
-if __name__ == '__main__':
-    args = sys.argv
-
-some_file = open("/usr/share/dict/words", "r")
-
+some_file = open("/Users/vladyslav/Desktop/cs12/words.txt", "r")
 some_lines = some_file.readlines()
 
-ran = randint(0, len(some_lines) - 1)
-ind = some_lines[ran]
-print(ind)
 
-def ran_words(ran):
-    words = []
-    for i in range(ran):
-        # number = int(sys.argv[1])
-
-
-if __name__ == '__main__':
-    print(ran_words(ran))
+for word in some_lines:
+    ran = randint(0, len(some_lines) - 1)
+    ind = some_lines[ran]
+    print(ind)
+    
