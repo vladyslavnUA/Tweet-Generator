d4c53190c9e5908a3de573c3113689f8f723e1be
vladyslavnUA <vladyslavn@yahoo.com>
2020-02-17 14:50:40 -0800

    updated dictionary_words.py

--- dictionary_words.py
+++ dictionary_words.py
@@ -1,19 +1,3 @@
-# import sys
-# from random import random, randint
-
-# # if __name__ == '__main__':
-# #     args = sys.argv
-
-# some_file = open("/usr/share/dict/words", "r")
-# some_lines = some_file.readlines()
-
-# for word in some_lines:
-#     ran = randint(0, len(some_lines) - 1)
-#     ind = some_lines[ran]
-#     # ind.append(ran)
-#     print(" ".join(ind[0:-1] for word in some_lines))
-#     some_file.close()
-
 import sys
 from random import random, randint
 
@@ -25,9 +9,3 @@ for word in some_lines:
     ran = randint(0, len(some_lines) - 1)
     ind = some_lines[ran]
     print(ind)
-    # print(" ".join(ind[0:-1] for word in some_lines))
-    # some_file.close()
-
-
-# if __name__ == '__main__':
-#     print(ran_words(ran))
