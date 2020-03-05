7867cd4b08bf69b7c472d288c9e065ab6073cc9a
vladyslavnUA <vladyslavn@yahoo.com>
2020-03-05 03:32:30 -0800

    updated app.py

--- app.py
+++ app.py
@@ -1,83 +1,124 @@
 from flask import Flask, render_template, request, redirect, url_for
-from pymongo import MongoClient
-from bson.objectid import ObjectId
 import os
+import random 
+from histogram.histogram import Histogram
+from histogram.sample import sample, words_list
+from histogram.dictogram import Dictogram
 
-host = os.environ.get('MONGODB_URI', 'mongodb://heroku_rkfkkcbg:u7ej95kepo531llkpkjttvs10l@ds361768.mlab.com:61768/heroku_rkfkkcbg')
-client = MongoClient(host=host)
-db = client.get_default_database()
-pools = db.pools
 
-#heroku doesnt know library
 app = Flask(__name__)
 
-# @app.route("/login")
-# def login:
-# 	auth = request.authorization
-# 	return " "
 
-@app.route("/")
-def pools_index():
-	#this will show the type of pools we have
-		#this will show the type of pants we have
-	return render_template("base.html", pools=pools.find())
+def get_sentence_from_histogram(count=50):
+    dictonary_words = Histogram.histogram()
+    sentence = ''
+    navBar_message = ''
 
-@app.route("/about")
-def about():
-	return render_template("about.html")
+    while count > 0:
+        sentence += sample(dictonary_words)+" "
+        if count < 10:
+            navBar_message += sample(dictonary_words)+" "
 
-@app.route("/pools", methods=["POST"])
-def pools_submit():
-	pool = {
-		"title": request.form.get("title"),
-		"size": request.form.get("size"),
-		"description": request.form.get("description"),
-		"price": request.form.get("price"),
-		"guests": request.form.get("guests"),
-		"picture": request.form.get("picture")
-	}
-	pool_id = pools.insert_one(pool).inserted_id
-	print(f"added pool id: {pool_id}")
-	return redirect(url_for("pools_show", pool_id = pool_id))
+        count -= 1
+    return [sentence, navBar_message]
 
-@app.route("/pools/<pool_id>")
-def pools_show(pool_id):
-	pool = pools.find_one({'_id' : ObjectId(pool_id)})
-	return render_template("pools_show.html", pool=pool, pool_id = pool_id)
 
-@app.route("/pools/login")
-def login():
-	return render_template("login.html")
 
-@app.route("/pools/new")
-def pools_new():
-	return render_template("pools_new.html", pool={}, title="new item")
+def build_markov(word_list):
+        markov_chain = {}
 
-@app.route("/pools/reserve")
-def pools_reserve():
-	return render_template("pools_reserve.html", pool={})
+        for i in range(len(word_list) - 1):
+            #get the current word and the word after
+            current_word = word_list[i]
+            next_word = word_list[i+1]
 
-@app.route("/pools/<pool_id>/edit")
-def pools_edit(pool_id):
-	pool = pools.find_one({"_id" : ObjectId(pool_id)})
-	return render_template("pools_edit.html", pool = pool, title="edit item")
+            if current_word in markov_chain.keys(): #already there
+                #get the histogram for that word in the chain
+                histogram = markov_chain[current_word]
+                #add to count
+                histogram[next_word] = histogram.get(next_word, 0) + 1
+            else: #first entry
+                markov_chain[current_word] = Dictogram([next_word]) 
 
-@app.route("/pools/<pool_id>", methods = ['POST'])
-def pools_update(pool_id):
-	updated_pool = {
-		"title": request.form.get("title"),
-		"size": request.form.get("size"),
-		"description": request.form.get("description"),
-		"price": request.form.get("price"),
-		"guests": request.form.get("guests")
-	}
-	pools.update_one( {"_id" : ObjectId(pool_id)}, {"$set" : updated_pool})
-	return redirect(url_for("pools_show", pool_id = pool_id))
+        return markov_chain
 
-@app.route("/pools/<pool_id>/delete", methods=["POST"])
-def pools_delete(pool_id):
-	pools.delete_one({"_id" : ObjectId(pool_id)})
-	return redirect(url_for('pools_index'))
+def walk(num_words, markov_chain, first_word):
+        #TODO: generate a sentence num_words long using the markov chain
+        sentence = ''
+        word = first_word
+        
+        # for i in range(0, num_words):
+        for i in range(num_words):
 
-if __name__ == "__main__":
-	app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
+            histogram = markov_chain[word]
+            next_word = sample(histogram)
+            sentence += next_word+" "
+            word = next_word
+        
+        return sentence
+
+
+def markov(num=0):
+
+    list_of_words = words_list()
+    # print(list_of_words)
+    first_word = random.choice(list_of_words)
+    
+    chain = build_markov(list_of_words)
+    if num == 0:
+        num = 10
+    sentence = walk(num, chain, first_word)
+
+    return sentence
+
+
+# helper function to get number 
+def check_number(num):
+    try:
+        count = int(num)
+        return count
+    except ValueError as verr:
+        print("Invalid number enter a number")
+        return False
+    except Exception as ex:
+        print(ex)
+        return False
+
+
+
+# home page markov default 10 words
+@app.route('/markov')
+@app.route('/')
+def home_route():
+    tweet = markov()
+    navBar_message = markov()
+    return render_template('index.html', tweet=tweet, navBar_message=navBar_message)
+
+# markov get n words
+@app.route('/markov/<num>')
+@app.route('/<num>')
+def dict_home_route(num):
+    count = check_number(num)
+    tweet = markov(count)
+    navBar_message = markov(10)
+    return render_template('index.html', tweet=tweet, navBar_message=navBar_message)
+
+
+# dictogram default 10 words
+@app.route('/dictogram')
+def dict_limit_words_route():
+    tweet = get_sentence_from_histogram(10)
+    navBar_message = get_sentence_from_histogram(random.randint(0, 10))
+    return render_template('index.html', tweet=tweet[0], navBar_message=navBar_message)
+
+
+# get n of words in a tweets 
+@app.route('/dictogram/<num>')
+def limit_words_route(num):
+    count = check_number(num)
+    tweet = get_sentence_from_histogram(count)
+    return render_template('index.html', tweet=tweet[0], navBar_message=tweet[1])
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
